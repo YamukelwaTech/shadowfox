961d5fad8ccda1c517a5dbfe0416bbc247fcf103
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-03-18 18:07:15 +0200

    done with tasks

--- Beginner/If Condition.py
+++ Beginner/If Condition.py
@@ -15,6 +15,53 @@ def BMI(height, weight):
         answer = "Underweight"
     return answer, bmi
 
+
 answer, calculated_bmi = BMI(height, weight)
 print("Your BMI answer is:", answer)
 print("Your calculated BMI value is:", calculated_bmi)
+
+# task 2
+Australia = ["Sydney", "Melbourne", "Brisbane", "Perth"]
+UAE = ["Dubai", "Abu Dhabi", "Sharjah", "Ajman"]
+India = ["Mumbai", "Bangalore", "Chennai", "Delhi"]
+
+city_name = input("Please enter a city name: ")
+
+
+def Country(city_name):
+    if city_name in Australia:
+        return f"{city_name} is in Australia"
+    elif city_name in UAE:
+        return f"{city_name} is in UAE"
+    elif city_name in India:
+        return f"{city_name} is in India"
+    else:
+        return "City not found in the lists."
+
+
+# calling the function
+print(Country(city_name))
+
+# task 3
+
+Australia = ["Sydney", "Melbourne", "Brisbane", "Perth"]
+UAE = ["Dubai", "Abu Dhabi", "Sharjah", "Ajman"]
+India = ["Mumbai", "Bangalore", "Chennai", "Delhi"]
+
+city_name = input("Please enter a city name: ")
+city_name_two = input("Please enter the second city name: ")
+
+
+def Country(city_name, city_name_two):
+    if city_name in Australia and city_name_two in Australia:
+        return "Both cities are in Australia"
+    elif city_name in UAE and city_name_two in UAE:
+        return "Both cities are in UAE"
+    elif city_name in India and city_name_two in India:
+        return "Both cities are in India"
+    else:
+        return "They do not belong in the same country"
+
+
+# calling the function
+print(Country(city_name, city_name_two))
